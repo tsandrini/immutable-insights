76e387d1d3c8c7dc211f26c499079f5bb9e60930
tsandrini <tomas.sandrini@seznam.cz>
2025-07-12 14:48:40 +0200

    feat(flake): remove _bootstrap in favor of builder.lib

--- flake.nix
+++ flake.nix
@@ -6,11 +6,11 @@
     # --- BASE DEPENDENCIES ---
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     flake-parts.url = "github:hercules-ci/flake-parts";
+    flake-parts-builder.url = "github:tsandrini/flake-parts-builder";
 
-    # --- YOUR DEPENDENCIES --- 
+    # --- YOUR DEPENDENCIES ---
     pre-commit-hooks.url = "github:cachix/pre-commit-hooks.nix";
     systems.url = "github:nix-systems/default";
-    treefmt-nix.url = "github:numtide/treefmt-nix";
     gitignore-nix.url = "github:hercules-ci/gitignore.nix";
   };
 
@@ -22,23 +22,20 @@
       "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
       "pre-commit-hooks.cachix.org-1:Pkk3Panw5AW24TOv6kz3PvLhlH8puAsJTBbOPmBo7Rc="
-      "numtide.cachix.org-1:2ps1kLBUWjxIneOy1Ik6cQjb41X0iXVXeHigGmycPPE="
       "tsandrini.cachix.org-1:t0AzIUglIqwiY+vz/WRWXrOkDZN8TwY3gk+n+UDt4gw="
     ];
     extra-substituters = [
       "https://cache.nixos.org"
       "https://nix-community.cachix.org/"
       "https://pre-commit-hooks.cachix.org/"
-      "https://numtide.cachix.org"
       "https://tsandrini.cachix.org"
     ];
   };
 
   outputs =
-    inputs@{ flake-parts, ... }:
+    inputs@{ flake-parts, flake-parts-builder, ... }:
     let
-      inherit (inputs.nixpkgs) lib;
-      inherit (import ./flake-parts/_bootstrap.nix { inherit lib; }) loadParts;
+      inherit (flake-parts-builder.lib) loadParts;
     in
     flake-parts.lib.mkFlake { inherit inputs; } {
       # We recursively traverse all of the flakeModules in ./flake-parts and

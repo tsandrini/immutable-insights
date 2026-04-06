7345079cefad2c3d26225c04e0fe1057888e7b43
tsandrini <t@tsandrini.sh>
2026-04-06 10:12:53 +0200

    feat(layouts/BaseLayout): tweak shiki code blocks

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,17 @@
+import mdx from "@astrojs/mdx";
 import { defineConfig } from "astro/config";
 
 export default defineConfig({
+	integrations: [mdx()],
+	markdown: {
+		shikiConfig: {
+			themes: {
+				light: "vitesse-light",
+				dark: "vitesse-dark",
+			},
+			defaultColor: false,
+		},
+	},
 	vite: {
 		server: {
 			watch: {
